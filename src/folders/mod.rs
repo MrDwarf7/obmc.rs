92cf19ef3d97a03fa384b4f3effe829b791297da
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-10-10 22:02:21 +1100

    ref(folders/mod): prefer generics

--- src/folders/mod.rs
+++ src/folders/mod.rs
@@ -1,20 +1,17 @@
 mod crawlers;
-mod folders;
+mod folder_types;
 
 use std::path::Path;
 
 // For internal use by crawl_dir
 use crawlers::{crawl_dir_recursive, crawl_dir_recursive_par};
 use eyre::Result;
-pub use folders::{Folder, Folders, ValidFileTypes, VideoType};
+pub use folder_types::{Folder, Folders, ValidFileTypes, VideoType};
 
 use crate::CrawlType;
 
 // Could hand in an enum of 'CrawlType' to determine what type to do, recursive or non-recursive // parallel or serial
-pub fn crawl_dir(
-    root: &Path,
-    crawl_type: CrawlType,
-) -> Result<Folders> {
+pub fn crawl_dir<P: AsRef<Path>>(root: &P, crawl_type: CrawlType) -> Result<Folders> {
     let root_folder = match crawl_type {
         CrawlType::Serial => crawl_dir_recursive(root)?,
         CrawlType::Parallel => crawl_dir_recursive_par(root)?,
